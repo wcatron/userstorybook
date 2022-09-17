f2ceec7c357e1c12fe1856de289b7418d38a1371
Weston Catron <weston.catron@somoglobal.com>
2022-09-17 14:39:28 -0400

    feat: add storybook to cli package

--- packages/cli/src/useCases/buildUserStoryBook.ts
+++ packages/cli/src/useCases/buildUserStoryBook.ts
@@ -3,17 +3,17 @@ import { join } from 'node:path'
 import { Project } from 'ts-morph'
 import { ParsedUseCase, parseUseCase } from '../utils/useCases'
 
-export interface FileDataSourceI {
+export interface FileDataSource {
   // Should make the directory if it doesn't exist or empty it if it does
   prepDirectory: (directory: string) => Promise<void>;
   writeFile: (filePath: string, contents: string) => Promise<void>;
 }
 
-interface LogInterface {
+interface Logger {
   log: (message: string) => void;
 }
 
-export interface TemplatesDataSourceI {
+export interface TemplatesDataSource {
   generate: ({
     useCase,
   }: {
@@ -37,40 +37,40 @@ export type Flags = {
 export const buildUserStoryBook: UseCase<
   { config: Config; flags: Flags },
   {
-    files: FileDataSourceI;
-    logger: LogInterface;
-    templates: TemplatesDataSourceI;
+    files: FileDataSource;
+    logger: Logger;
+    templates: TemplatesDataSource;
   },
   Promise<void>
-> = async (
+> = async function (
   { config: { root, skip, output }, flags: { verbose, jsonOnly } },
   { files, templates, logger },
-) => {
-  logger.log(`Loading use cases from '${process.cwd()}/${root}'`)
-  // TODO: Add tsConfigFilePath back, currently it causes imports to be added in front of types
-  const project = new Project()
-  const useCases = project.addSourceFilesAtPaths(`${root}/**`).map(parseUseCase(skip))
-  .filter((a): a is ParsedUseCase => a !== undefined)
+) {
+    logger.log(`Loading use cases from '${process.cwd()}/${root}'`)
+    // TODO: Add tsConfigFilePath back, currently it causes imports to be added in front of types
+    const project = new Project()
+    const useCases = project.addSourceFilesAtPaths(`${root}/**`).map(parseUseCase(skip))
+      .filter((a): a is ParsedUseCase => a !== undefined)
 
-  if (verbose) console.log(JSON.stringify(useCases, undefined, 2))
+    if (verbose) console.log(JSON.stringify(useCases, undefined, 2))
 
-  await files.prepDirectory(output)
+    await files.prepDirectory(output)
 
-  files.writeFile(join(output, 'output.json'), JSON.stringify(useCases, undefined, 4))
+    files.writeFile(join(output, 'output.json'), JSON.stringify(useCases, undefined, 4))
 
-  if (jsonOnly) {
-    return
+    if (jsonOnly) {
+      return
+    }
+
+    await Promise.allSettled(useCases.map(async useCase => {
+      const result = templates.generate({
+        useCase,
+        useCases,
+      })
+      const filePath = join(output, `${useCase.name}.html`)
+      if (verbose)
+        console.log(`Generated result for ${useCase.namePretty}: ${filePath}`)
+
+      await files.writeFile(filePath, result)
+    }))
   }
-
-  await Promise.allSettled(useCases.map(async useCase => {
-    const result = templates.generate({
-      useCase,
-      useCases,
-    })
-    const filePath = join(output, `${useCase.name}.html`)
-    if (verbose)
-      console.log(`Generated result for ${useCase.namePretty}: ${filePath}`)
-
-    await files.writeFile(filePath, result)
-  }))
-}
